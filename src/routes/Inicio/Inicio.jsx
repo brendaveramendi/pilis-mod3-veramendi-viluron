14b4844d09980d9ab51550a6e5231c7f1a6c54d7
Brenda Veramendi <brendaveramendi@gmail.com>
2022-10-13 16:02:22 -0300

    estilos

--- src/routes/Inicio/Inicio.jsx
+++ src/routes/Inicio/Inicio.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
+import './Inicio.css'
+
 
 function Inicio() {
+  const { setCurrentUser } = useContext(UserContext)
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const navigate = useNavigate()
+
+  const onSubmit = (data) => {
+    localStorage.setItem('currentUser', JSON.stringify(data))
+    setCurrentUser(data)
+    navigate('/')
+  }
   return (
-    <div>
-        <h2>Inicio</h2>
-    </div>
+    <div className='form-container'>
+      <form className='form' onSubmit={handleSubmit(onSubmit)}>
+        <span className='form-title'>Bienvenido</span>
+     <input
+        type="text"
+        className='form-input'
+        placeholder='Nombre de usuario'
+        {...register('username', {
+            required : 'Debe ingresar su nombre de usuario'
+          }
+        )
+      } />
+      <p>{errors.username?.message}</p>
+     
+     <input
+        type='password'
+        className='form-input'
+        placeholder='Contraseña'
+        {...register(
+            'password',
+            {
+              required : 'Debe ingresar su contraseña'
+            }
+          )
+        }
+     />
+        <p>{errors.password?.message}</p>
+      
+       <button type='submit' className='form-submit'>Iniciar Sesión</button>
+    </form>
+   </div>
   )
 }
 
 
-export default Inicio
+export default Inicio;
