35464e3ba1e2e5c96e9f1e9fafa27b2752272bac
Brenda Veramendi <brendaveramendi@gmail.com>
2022-10-13 15:49:22 -0300

    context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import { createContext, useState } from "react";
+import './App.css';
+import { useEffect, useContext } from 'react';
+import { Routes, Route} from 'react-router-dom';
+import { getCards } from './services';
+import Navigation from './routes/Navigation/Navigation'
+import Home from './routes/Home/Home';
+import Formulario from './routes/Formulario/Formulario';
+import Inicio from './routes/Inicio/Inicio';
+import {CardContext} from './context/CardContext';
+//import AppLogo from './AppLogo.png'
 
-export const UserContext = createContext({
-  currentUser: {},
-  setCurrentUser: () => {}
-})
+function App() {
+  const {setCardsColection } = useContext(CardContext)
+  useEffect(() => {
+    getCards()
+    .then(data=>setCardsColection(data))
+    .catch(err=> console.log(err))
+  }, [])
 
-export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
-
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+    return (
+    <>   
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Navigation />} >  
+          <Route index element={<Home />} />  
+          <Route path='/inicio' element={<Inicio />} />
+          <Route path='/formulario' element={<Formulario />} /> 
+        </Route>
+       </Routes>
+    </div>   
+    </>
+   
+  );
 }
+
+export default App;
